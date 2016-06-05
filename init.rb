e890071d90bbc362462d12eb51ff35d4eccacfb6
yama07 <n.116@icloud.com>
2016-06-05 17:08:48 +0900

    update init.rb

--- init.rb
+++ init.rb
@@ -3,10 +3,9 @@ require 'export_with_journals'
 
 Redmine::Plugin.register :redmine_export_with_journals do
   name 'Redmine Export With Journals plugin'
-  author 'suer'
-  description 'Export CSV with Journals'
+  author 'yama07'
+  description 'Export CSV with Journals and Changesets'
   version '0.0.7'
-  url 'https://github.com/suer/redmine_export_with_journals'
-  author_url 'http://d.hatena.ne.jp/suer'
+  url 'https://github.com/yama07/redmine_export_with_journals.git'
   permission :export_csv_with_journals, {:issues_export => :export_with_journals}
 end
